6cc84928258e5606b7b10f77e414be2a9dda85e5
Dao Duy Quyen <quyenne3704@gmail.com>
2025-09-14 23:13:14 +0700

    create project and pages LMS

---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../common/Layout'
+
+const Home = () => {
+    return (
+        <div>
+            <Layout>
+                <h1>Home</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default Home
---
+++ frontend/src/components/pages/account/CoursesEnrolled.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesEnrolled = () => {
+    return (
+        <div>
+            <h1>CoursesEnrolled</h1>
+        </div>
+    )
+}
+
+export default CoursesEnrolled
